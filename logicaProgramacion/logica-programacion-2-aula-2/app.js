593eb1abfed854cde128d00366246510ed34ee7e
victor-huallpa <vech.600@gmail.com>
2025-02-13 21:49:22 -0500

    PARTE 1

---
+++ logicaProgramacion/logica-programacion-2-aula-2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+function IntentoDeUsuario(){
+    alert('clic desde la funcion');
+}
