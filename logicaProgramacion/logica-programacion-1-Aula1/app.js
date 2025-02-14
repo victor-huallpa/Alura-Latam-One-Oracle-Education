6b9a352b685c4881667f1de106f9f2441016bf2e
victor-huallpa <vech.600@gmail.com>
2025-02-13 21:11:21 -0500

    CLASE FINALIZADA

--- logicaProgramacion/logica-programacion-1-Aula1/app.js
+++ logicaProgramacion/logica-programacion-1-Aula1/app.js
@@ -1,14 +1,36 @@
-//varaibles
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+//Variables
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 6;
 
-//mostrar por pantalla
-console.log(numeroUsuario);
 
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
 
-//validar con la condiiconal if
-if (numeroUsuario == numeroSecreto) {
-    alert(`Acertaste, el numero es: ${numeroSecreto}`);
-}else{
-    alert('Lo siento no acertaste con el numero');
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
 }
