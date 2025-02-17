b1240a019583a2625ca2b607d32a4639e314adea
victor-huallpa <vech.600@gmail.com>
2025-02-16 22:19:09 -0500

    ARTE DOS

--- logicaProgramacion/logica-programacion-2-aula-2/app.js
+++ logicaProgramacion/logica-programacion-2-aula-2/app.js
@@ -1,9 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto';
+let numeroSecreto = generarNumeroSecreto();
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un numero del 1 al 10';
+function asignatTextoElemento (elemento, texto){
+    let titulo = document.querySelector(elemento);
+    titulo.innerHTML = texto;
+    return;
+}
 
 function IntentoDeUsuario(){
     alert('clic desde la funcion');
 }
+
+function generarNumeroSecreto() {
+    let numeroSecreto = Math.floor(Math.random()*10+1)   
+    return numeroSecreto;
+}
+
+asignatTextoElemento('h1', 'Juego del numer osecreto');
+asignatTextoElemento('p', 'Indica un numero del 1 al 10');
